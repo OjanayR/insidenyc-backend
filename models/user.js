24c64aedacaed79378a6543c0012ca66f770947b
OjanayR <69225985+OjanayR@users.noreply.github.com>
2020-12-12 15:19:36 -0500

    installed jsonwebtoken

--- models/user.js
+++ models/user.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt');
+const SALT_ROUNDS = 6;
 
 const userSchema = new Schema({
   name: String,
@@ -21,5 +23,21 @@ userSchema.set('toJSON', {
     return ret;
   }
 });
+
+userSchema.pre("save", function (next) {
+  const user = this;
+  if (!user.isModified("password")) return next();
+  // password has been changed - salt and hash it
+  bcrypt.hash(user.password, SALT_ROUNDS, function (err, hash) {
+    if (err) return next(err);
+    // replace the user provided password with the hash
+    user.password = hash;
+    next();
+  });
+});
+
+userSchema.methods.comparePassword = function (tryPassword, cb) {
+  bcrypt.compare(tryPassword, this.password, cb);
+};
 
 module.exports = mongoose.model('User', userSchema);
